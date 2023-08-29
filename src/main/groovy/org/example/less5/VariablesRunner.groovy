fa28be121698adf45c32501ed3d110bf13de73aa
yolkaSD <yolka.sd@gmail.com>
2023-08-29 16:06:41 +0700

    Get familiar with groovy variables

---
+++ src/main/groovy/org/example/less5/VariablesRunner.groovy
@@ -0,0 +1,14 @@
+package org.example.less5
+
+import groovy.transform.CompileStatic
+
+import java.sql.Date as SqlDate
+
+//@CompileStatic
+class VariablesRunner {
+    static void main(String[] args) {
+        int value1 = 5 // Integer value = 5
+        def value2 = 10 // Integer value = 5
+        new SqlDate();
+    }
+}
